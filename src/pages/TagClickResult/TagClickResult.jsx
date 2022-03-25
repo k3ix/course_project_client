a555fcd85b9eb4a10a3df1aad40b415186fdee57
k3ix <egormishuk@gmail.com>
2022-03-25 20:25:36 +0300

    redirect + tag result

--- src/pages/TagClickResult/TagClickResult.jsx
+++ src/pages/TagClickResult/TagClickResult.jsx
@@ -1,6 +1,8 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate, useParams} from "react-router-dom";
-import {getAllOverviewsApi} from "../../api";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from "react-router-dom";
+import { getAllOverviewsApi } from "../../api";
+import { Box, Button, Container, Grid } from "@mui/material";
+import { FavoriteBorder } from "@mui/icons-material";
 
 export const TagClickResult = () => {
     let { tag } = useParams();
@@ -15,10 +17,19 @@ export const TagClickResult = () => {
             }).sort((a, b) => {
                 return (b.id - a.id);
             });
-           setListOfAllOverviews(filteredOverviews);
-           setListOfCurrOverviews(filteredOverviews.slice(0, 5));
+            console.log(filteredOverviews);
+            setListOfAllOverviews(filteredOverviews);
+            setListOfCurrOverviews(filteredOverviews.slice(0, 5));
         });
     }, [tag]);
+
+    const showMoreBtn = () => {
+        if(listOfCurrOverviews.length + 5 >= listOfAllOverviews.length){
+            setListOfCurrOverviews(listOfAllOverviews);
+        } else{
+            setListOfCurrOverviews(listOfAllOverviews.slice(0, listOfCurrOverviews.length + 5))
+        }
+    };
 
     return (
         <div className="tag-click-result">
@@ -30,6 +41,40 @@ export const TagClickResult = () => {
                     {"No results for tag " + "\"#" + tag + "\""}
                 </div>
             }
+            <Container maxWidth="xs" >
+                <Grid item container direction="column" justifyContent="center" spacing={1}>
+                    {listOfCurrOverviews.map((value, key) => {
+                        const thisTags = value.tags.split(" ").slice(0, -1);
+                        return (
+                            <Grid item key={key} container>
+                                <Box className="item">
+                                    <header onClick={() => {history(`/overview/${value.id}`)}}>
+                                        {value.title}
+                                    </header>
+                                    <Grid item container justifyContent="flex-start" alignItems="flex-start">
+                                        {thisTags.map((value, key) => {
+                                            return(
+                                                <Grid item key={key} onClick={() => {history(`/by-tag/${value}`)}} className="itemTag" style={{ margin: "6px" }}>
+                                                    {"#" + value}
+                                                </Grid>
+                                            )
+                                        })}
+                                    </Grid>
+                                    <footer>
+                                        <FavoriteBorder style={{ color: "blue" }} />
+                                        {value.Likes.length}
+                                    </footer>
+                                </Box>
+                            </Grid>
+                        )
+                    })}
+                    {!(listOfCurrOverviews.length >= listOfAllOverviews.length) &&
+                        <Button onClick={showMoreBtn} id="submitButton" style={{ marginBottom: 8 }}>
+                            Show more
+                        </Button>
+                    }
+                </Grid>
+            </Container>
         </div>
     );
 };
